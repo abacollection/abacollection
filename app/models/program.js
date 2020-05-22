ec45aae9a97f712a2cbce705c1f27b72675d5a46
shadowgate15 <taylorschley25@gmail.com>
2020-05-21 20:02:50 -0500

    feat: added Program model

---
+++ app/models/program.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const mongooseCommonPlugin = require('mongoose-common-plugin');
+
+// <https://github.com/Automattic/mongoose/issues/5534>
+mongoose.Error.messages = require('@ladjs/mongoose-error-messages');
+
+const Program = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    index:true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  creation_date: {
+    type: Date,
+    required: true
+  },
+  created_by: [
+    { type: mongoose.Schema.ObjectId, ref: 'User'}
+  ],
+  client: [
+    { type: mongoose.Schema.ObjectId, ref: 'Client'}
+  ]
+});
+
+Program.plugin(mongooseCommonPlugin, { object: 'program'});
+
+module.exports = mongoose.model('Program', Program);
+
