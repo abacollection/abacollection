c563f195e718075e9f8866524493a47c8a7bc72b
shadowgate15 <taylorschley25@gmail.com>
2020-06-15 16:55:17 -0500

    fix: fixed random syntax errors

--- routes/web/dashboard.js
+++ routes/web/dashboard.js
@@ -14,7 +14,7 @@ router.get('/', render('dashboard'));
 router.get('/clients',
   paginate.middleware(10, 50),
   web.dashboard.clients.list);
-router.post('/clients', web.dashboard.clients.add_client);
+router.put('/clients', web.dashboard.clients.add_client);
 router.delete('/clients/:client_id',
   web.dashboard.clients.retrieveClient,
   web.dashboard.clients.delete_client);
