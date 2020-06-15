11844ace5f5b615ad868fa4185f5169e470a7265
shadowgate15 <taylorschley25@gmail.com>
2020-06-15 09:57:08 -0500

    test: add placeholders for tests

---
+++ test/web/dashboard/clients.js
@@ -0,0 +1,8 @@
+const test = require('ava');
+
+test.todo('successfully create client');
+test.todo('must have first name to create client');
+test.todo('must have last name to create client');
+test.todo('only see clients user is a member of');
+test.todo('successfully delete client if admin');
+test.todo('unsuccessfully delete client if not admin');
