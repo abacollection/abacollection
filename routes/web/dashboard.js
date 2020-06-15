a7bb6502af21cc99aea19ca7f0dfae66ee67bacc
shadowgate15 <taylorschley25@gmail.com>
2020-06-15 08:30:47 -0500

    fix: fixed delete client button

--- routes/web/dashboard.js
+++ routes/web/dashboard.js
@@ -9,10 +9,10 @@ const router = new Router({ prefix: '/dashboard' });
 
 router.use(policies.ensureLoggedIn);
 router.use(web.breadcrumbs);
+router.use(web.dashboard.clients.retrieveClients);
 router.get('/', render('dashboard'));
 router.get('/clients',
   paginate.middleware(10, 50),
-  web.dashboard.clients.retrieveClients,
   web.dashboard.clients.list);
 router.post('/clients', web.dashboard.clients.add_client);
 router.delete('/clients/:client_id',
